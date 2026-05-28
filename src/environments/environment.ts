4d2b814a5da4338ccc602c3c0a4a90b813879ada
augsto zagnoli <pablosz@sefaz.rs.gov.br>
2026-05-27 23:58:42 -0300

    env prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:7233'
+// };
+
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7233'
+  apiUrl: 'https://producaovemexhidraulica.tech:5002'
 };
